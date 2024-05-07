2b87ae2f40a43a580f41a0eb7aec0dfcba186ff8
hadeer <hadeer.abelgawad44@gmail.com>
2024-05-07 03:38:13 +0300

    modify4

---
+++ js/employeeData.js
@@ -0,0 +1,56 @@
+// let emRow = document.querySelector('.row')
+
+// let Data=[]
+// // employeeData.push('nothing')
+
+// class employeesData{
+//     constructor(name,img){
+//         this.name=name
+//         this.img=img
+//         Data.push(this)
+//     }
+// }
+
+// const data1= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data2= new employeesData('Mostafa Yasmen','images/download.jpeg');
+// const data3= new employeesData('Hadeer Emad','images/download.jpeg');
+// const data4= new employeesData('Shahd Elhl','images/download.jpeg');
+// const data5= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data6= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data7= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data8= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data9= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data10= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data11= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data12= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data13= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data14= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data15= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data16= new employeesData('Ahmed Mohamed','images/download.jpeg');
+// const data17= new employeesData('Ahmed Mohamed','images/download.jpeg');
+
+// function draw(){
+//     for(let i=1;i<Data.length;i++){
+//         emRow.innerHTML+=`
+//         <div class="card col-lg-4 col-md-6 col-sm-12 mt-1 mb-1" style="width:250px; height: 250; margin-bottom:10px; margin-right:2px; margin:auto">
+//                 <img class="card-img-top" src="${Data[i].img}" alt="Card image" style="width:100%">
+//                 <div class="card-body" style="height: 100px;">
+//                     <h6 class="card-title">${Data[i].name}</h6>
+//                     <a href="#" class="btn btn-primary" onclick='show()'>See Profile</a>
+//                 </div>
+//         </div>
+//         `
+//     }
+// }
+// draw();
+
+// function show(){
+
+// }
+
+// let showBtn = document.querySelector(".showBtn")
+// showBtn.addEventListener("click", function(){
+//     setTimeout(()=>{
+//         window.location="showData.html"
+//     },1000)
+// })
